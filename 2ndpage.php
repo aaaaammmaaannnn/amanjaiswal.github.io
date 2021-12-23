e66fecf37160d102da10a9a55ef98a4cd18c66b3
aaaaammmaaannnn <aaman.jaiswal.75@gmail.com>
2021-12-23 13:58:54 +0530

    hello

---
+++ 2ndpage.php
@@ -0,0 +1,38 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Schedule</title>
+	<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.0.1/fullcalendar.css">
+	<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.0.3/css/bootstrap-theme.css">
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.29.1/moment.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.10.2/fullcalendar.min.js"></script>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+<link href="https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.0.1/fullcalendar.css" rel="stylesheet" />
+<script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.15.1/moment.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.10.3/jquery-ui.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.0.1/fullcalendar.js"></script>
+<div id="calendar"></div>
+
+	<script> 
+	$('#calendar').fullCalendar({
+  defaultView: 'agendaWeek'
+});
+
+	</script>
+
+</head>
+<body>
+	<br />
+	<h2 align="center"><a href="#">Jquery Fullcalender Integration with PHP and Mysql</a></h2>
+	<br />
+	<div class="container"></div>
+		<div id="calender"></div>
+	</div>
+
+	
+</body>
+</html>
